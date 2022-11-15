d6a9a94c2140bd9e188e075fb7c020d9693f5d6c
shan2305 <la207980@student.helha.be>
2022-11-15 12:53:58 +0100

    connextion front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './index-user/user-list/user-list.component';
+import { UserCreateComponent } from './index-user/user-create/user-create.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import { IndexUserComponent } from './index-user/index-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserCreateComponent,
+    IndexUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
