c1a5381102b83386e82a0984a88b204f91d164b7
Mathis <Mathisbruyere@hotmail.com>
2022-12-17 20:52:35 +0100

    page accueil responsive

--- src/app/index-user/user-profile/user-profile.component.ts
+++ src/app/index-user/user-profile/user-profile.component.ts
@@ -1,9 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit} from '@angular/core';
 import {DtoInputUser} from "../dto/dto-input-user";
 import {UserService} from "../user.service";
 import {DtoCreateArticle} from "../../index-articles/dto/dto-create-article";
 import {DtoInputArticle} from "../../index-articles/dto/dtoInputArticle";
 import {ArticlesService} from "../../index-articles/articles.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {CategoryService} from "../../index-category/category.service";
+import {DtoInputCategory} from "../../index-category/dto/dto-input-category";
 
 @Component({
   selector: 'app-user-profile',
@@ -13,16 +16,41 @@ import {ArticlesService} from "../../index-articles/articles.service";
 export class UserProfileComponent implements OnInit {
   user: DtoInputUser | null = null;
   articles: DtoInputArticle[] = [];
+  categories: DtoInputCategory[]=[];
 
-  constructor(private _userService: UserService, private _articleService : ArticlesService) {
+  articleCreated  :DtoCreateArticle | null = null;
+
+  form : FormGroup = this.fb.group({
+    name: this.fb.control("", Validators.required),
+    urlImage: this.fb.control("", Validators.required),
+    nomCat: this.fb.control("", Validators.required),
+    description : this.fb.control("",Validators.required),
+    combobox: this.fb.control("",Validators.required)
+  });
+
+  constructor(private _userService: UserService,
+              private _articleService : ArticlesService,
+              private fb: FormBuilder,
+              private _categoryService : CategoryService) {
 
   }
 
   ngOnInit(): void {
     this.fetchByIdToken();
     this.fetchAllArticleByUserId();
+    this.fetchAllCategorie();
   }
 
+  sendArticle() {
+  this.articleCreated =
+    {
+      name : this.form.value.name,
+      urlImage : this.form.value.urlImage,
+      nomCat : this.form.value.combobox,
+      description : this.form.value.description
+    }
+    this.createArticle(this.articleCreated);
+  }
   fetchByIdToken() {
     return this._userService
       .fetchByIdToken()
@@ -40,5 +68,13 @@ export class UserProfileComponent implements OnInit {
 
   createArticle(dto: DtoCreateArticle) {
     this._articleService.create(dto).subscribe(article => this.articles.push(article));
+  }
+  fetchAllCategorie() {
+    this._categoryService.fetchAll()
+      .subscribe(category => {
+        this.categories = category,
+          console.log(this.categories)
+      });
+
   }
 }
